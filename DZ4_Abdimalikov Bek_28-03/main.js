48fd10e202028b8cb1cb245e28360c04d70aea57
Pipmor <139385966+Pipmor@users.noreply.github.com>
2023-07-23 15:35:27 +0600

    superrrrr

--- DZ4_Abdimalikov Bek_28-03/main.js
+++ DZ4_Abdimalikov Bek_28-03/main.js
@@ -6,28 +6,33 @@ changeColorBtn.addEventListener('click', () => {
     const userInput = prompt('Введите цвет: "Красный", "Желтый" или "Зеленый"');
     if (userInput) {
         const color = userInput.toLowerCase();
+        let message;
         switch (color) {
             case 'красный':
                 lightColor = 'red';
+                message = 'Стоп';
                 break;
             case 'желтый':
                 lightColor = 'yellow';
+                message = 'Подождите';
                 break;
             case 'зеленый':
                 lightColor = 'green';
+                message = 'Идите';
                 break;
             default:
-                return alert('Error');
-
+                alert('Некорректный ввод! Введите "Красный", "Желтый" или "Зеленый".');
+                return;
         }
-        updateTrafficLight();
+        updateTrafficLight(message);
     }
 });
 
-function updateTrafficLight() {
+function updateTrafficLight(message) {
     const lights = trafficLight.querySelectorAll('.light');
     lights.forEach((light) => {
         light.classList.remove('active');
     });
     trafficLight.querySelector(`.${lightColor}`).classList.add('active');
+    alert(`${lightColor.charAt(0).toUpperCase() + lightColor.slice(1)} - ${message}`);
 }
