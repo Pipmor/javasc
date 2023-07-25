997a2abd740c7578aa0a9fd6f516335fa39bf362
Pipmor <139385966+Pipmor@users.noreply.github.com>
2023-07-25 16:23:07 +0600

    yhohohoohohoh

---
+++ DZ5_Abdimalikov Bek_28-03/main.js
@@ -0,0 +1,28 @@
+const numbers = [3, 4, 5, 44, 55, 23, 34, 12, 26, 67, 77, 75, 98, 67];
+
+const oddNumbers = numbers.filter((number) => number % 2 !== 0);
+
+oddNumbers.forEach((number) => {
+    console.log(number);
+});
+
+
+
+
+function customFilterBooks(...books) {
+    const booksWithU = [];
+    const otherBooks = [];
+
+    for (const book of books) {
+        if (book.includes('у') || book.includes('У')) {
+            booksWithU.push(book);
+        } else {
+            otherBooks.push(book);
+        }
+    }
+
+    console.log('Книги с буквой "у" или "Y":', booksWithU);
+    console.log('Остальные книги:', otherBooks);
+}
+
+customFilterBooks('Путешествие к центру Земли', 'Гарри Поттер и Философский камень', 'Зов Ктулху', 'Мастер и Маргарита', 'Мертвые души', 'Маленький принц', 'Убик', 'Хроники Нарнии',);
